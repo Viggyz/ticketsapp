c05e0e25235bca212aef40c0a12c7f5a190c3f06
Viggy <jinshadow17@gmail.com>
2022-01-15 19:03:38 +0530

    Course Section 7 Complete

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "./custom-error";
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+
+  console.log("Something went wrong", err);
+
+  res.status(400).send({ errors: { message: "Something went wrong" } });
+}
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../middlewares/request-validation-error";
+import { DatabaseConnectionError } from "../middlewares/database-connection-error";
+
+const router = express.Router();
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+      // return res.status(400).send(errors.array())
+    }
+
+    const { email, password } = req.body;
+
+    console.log("Creating a user..");
+    throw new DatabaseConnectionError();
+    res.send({});
+  }
+);
+
+export { router as signupRouter };
