af8477202781c6ddec08de58754bef78bb45bece
Viggy <jinshadow17@gmail.com>
2022-01-17 00:01:06 +0530

    Section 10 complete

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,50 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns 400 with an invalid password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  return request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "passedword",
+    })
+    .expect(400);
+});
+
+it("responds with a cookie when given valid credentials", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  const response = await request(app) //This await is waaay too important
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
