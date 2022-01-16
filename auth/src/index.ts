93720a98afe08b8a3916237f7a6954a68b07d8bf
Viggy <jinshadow17@gmail.com>
2022-01-16 15:42:47 +0530

    Section 8 complete

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -10,7 +12,14 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./middlewares/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -23,6 +32,17 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("connected to mongodb");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 app.listen(3000, () => {
   console.log("Listening on port 3000!!");
 });
+
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../middlewares/request-validation-error";
-import { DatabaseConnectionError } from "../middlewares/database-connection-error";
+import { BadRequestError } from "../middlewares/bad-request-error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -24,9 +25,16 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("Creating a user..");
-    throw new DatabaseConnectionError();
-    res.send({});
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email in use");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
