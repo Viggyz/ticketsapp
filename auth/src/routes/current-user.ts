ee1160e2e0813161927813c79b15ef7cda812268
Viggy <jinshadow17@gmail.com>
2022-01-18 22:14:41 +0530

    Section updated for new docker and npm account

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,5 @@
 import express from "express";
-
-import { currentUser } from "../middlewares/current-user";
+import { currentUser } from "@ticketingpack/common";
 
 const router = express.Router();
 
