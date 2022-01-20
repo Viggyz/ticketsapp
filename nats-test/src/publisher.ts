bc450a50c2627de774c9442f69553a1b6adcf8fb
Viggy <jinshadow17@gmail.com>
2022-01-20 21:32:49 +0530

    Section 13 and 14 complete - NATS basic

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,20 @@
+import nats from "node-nats-streaming";
+
+console.clear();
+
+const stan = nats.connect("ticketing", "abc", {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("Published connected to NATS");
+
+  const data = JSON.stringify({
+    title: "concert",
+    price: 20,
+  });
+
+  stan.publish("ticket:created", data, () => {
+    console.log("Event published");
+  });
+});
