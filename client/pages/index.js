bc9e6cbbe7fc30c102c010990e4156052f8f6552
Viggy <jinshadow17@gmail.com>
2022-01-26 23:29:04 +0530

    Section 22 - React Next Webapp complete

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,40 @@
-import buildClient from "../api/build-client";
+import Link from "next/link";
+const Index = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
+    );
+  });
 
-const Index = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in!</h1>
-  ) : (
-    <h1>You are NOT signed in</h1>
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
-Index.getInitialProps = async (context) => {
-  const { data } = await buildClient(context).get("/api/users/currentuser");
-  return data;
+Index.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get("/api/tickets");
+
+  return { tickets: data };
 };
 
 export default Index;
